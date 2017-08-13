16c8f0f87f2a9c1bcc9aaf8eb84e410f28a36df1
Guilherme Bernal <dev@lbguilherme.com>
2017-08-13 20:05:14 -0300

    Implement dsl support for map

--- src/reql/dsl.cr
+++ src/reql/dsl.cr
@@ -1,56 +1,80 @@
 module ReQL
   module DSL
     module R
+      @@next_var_i = 1i64
+      alias Type = Array(Type) | Bool | Float64 | Hash(String, Type) | Int64 | String | Nil | R
+
+      def self.convert_type(x : R)
+        x.val.as(Term::Type)
+      end
+
+      def self.convert_type(x : Term)
+        x.as(Term::Type)
+      end
+
+      def self.convert_type(x : Int)
+        x.to_i64.as(Term::Type)
+      end
+
+      def self.convert_type(x : Float)
+        x.to_f64.as(Term::Type)
+      end
+
+      def self.convert_type(x : Array)
+        x.map { |y| self.convert_type(y).as(Term::Type) }.as(Term::Type)
+      end
+
+      def self.convert_type(x : Hash)
+        h = {} of String => Term::Type
+        x.each do |(k, v)|
+          h[k] = self.convert_type v
+        end
+        h.as(Term::Type)
+      end
+
+      def self.convert_type(x : Bool | Float64 | Int64 | String | Term | Nil)
+        x.as(Term::Type)
+      end
+
+      def self.make_var_i
+        i = @@next_var_i
+        @@next_var_i += 1
+        i
+      end
+
+      getter val : Term::Type
+
+      def inspect(io)
+        @val.inspect io
+      end
+
+      def run
+        Evaluator.new.eval @val
+      end
     end
 
     def r
       R
     end
 
+    def r(val)
+      RExpr.new(val)
+    end
+
+    struct RExpr
+      include R
+
+      def initialize(val)
+        @val = R.convert_type(val)
+      end
+    end
+
     macro term(name, term_class)
       struct R{{name}}
         include R
 
-        getter term : Term
-
         def initialize(*args)
-          @term = {{term_class}}.new(args.to_a.map { |x| R{{name}}.convert_type(x).as(Term::Type) }, nil)
-        end
-
-        def self.convert_type(x : R)
-          x.term.as(Term::Type)
-        end
-
-        def self.convert_type(x : Int)
-          x.to_i64.as(Term::Type)
-        end
-
-        def self.convert_type(x : Float)
-          x.to_f64.as(Term::Type)
-        end
-
-        def self.convert_type(x : Array)
-          x.map { |y| self.convert_type y }.as(Term::Type)
-        end
-
-        def self.convert_type(x : Hash)
-          h = {} of String => Term::Type
-          x.each do |(k, v)|
-            h[k] = self.convert_type v
-          end
-          h.as(Term::Type)
-        end
-
-        def self.convert_type(x : Bool | Float64 | Int64 | String | Term | Nil)
-          x.as(Term::Type)
-        end
-
-        def inspect(io)
-          @term.inspect io
-        end
-
-        def run
-          Evaluator.new.eval @term
+          @val = {{term_class}}.new(args.to_a.map { |x| R.convert_type(x).as(Term::Type) }, nil)
         end
       end
 
@@ -62,6 +86,20 @@ module ReQL
         def {{name}}(*args)
           R{{name}}.new(self, *args)
         end
+
+        def self.{{name}}(*args, &block : R -> R::Type)
+          vari = {R.make_var_i}.map(&.as(Term::Type))
+          vars = vari.map { |i| RExpr.new(VarTerm.new([i], nil)).as(R) }
+          func = ReQL::FuncTerm.new([vari.to_a, R.convert_type(block.call(*vars))].map(&.as(Term::Type)), nil))
+          R{{name}}.new(*args, func)
+        end
+
+        def {{name}}(*args, &block : R -> R::Type)
+          vari = {R.make_var_i}.map(&.as(Term::Type))
+          vars = vari.map { |i| RExpr.new(VarTerm.new([i], nil)).as(R) }
+          func = ReQL::FuncTerm.new([vari.to_a, R.convert_type(block.call(*vars))].map(&.as(Term::Type)), nil)
+          R{{name}}.new(self, *args, func)
+        end
       end
     end
 
@@ -70,5 +108,6 @@ module ReQL
     term insert, InsertTerm
     term count, CountTerm
     term range, RangeTerm
+    term map, MapTerm
   end
 end
