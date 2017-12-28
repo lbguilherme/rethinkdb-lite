ccd6089e4ffc67dba320348e657be4dc385d2baa
Guilherme Bernal <dev@lbguilherme.com>
2017-12-28 08:57:32 -0300

    Add rethinkdb yaml suite to spec

---
+++ spec/rql_test/reql_spec.cr
@@ -0,0 +1,137 @@
+def match_reql_output(result)
+  matcher = with ReqlMatchers.new yield
+  recursive_match result, matcher
+end
+
+def recursive_match(result, target)
+  case target
+  when Matcher
+    target.match(result)
+  when Array
+    result.should be_a Array(ReQL::Datum::Type)
+    if result.is_a? Array
+      result.size.should eq target.size
+      result.size.times do |i|
+        recursive_match result[i], target[i]
+      end
+    end
+  when Hash
+    result.should be_a Hash(String, ReQL::Datum::Type)
+    if result.is_a? Hash
+      (result.keys - result.keys).size.should eq 0
+      result.keys.each do |key|
+        recursive_match result[key], target[key]
+      end
+    end
+  else
+    result.should eq target
+  end
+end
+
+def recursive_match(result : Array, target : Array)
+  result.should be_a Array(ReQL::Datum::Type)
+  result.size.should eq target.size
+  result.size.times do |i|
+    recursive_match result[i], target[i]
+  end
+end
+
+describe ReQL do
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/datum/array.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/datum/bool.yaml") }}
+  {{ run("./reql_spec_generator", "spec/rql_test/src/datum/null.yaml") }}
+  {{ run("./reql_spec_generator", "spec/rql_test/src/datum/number.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/datum/object.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/datum/string.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/datum/typeof.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/datum/uuid.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/add.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/aliases.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/div.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/floor_ceil_round.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/logic.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/math.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/mod.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/mul.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/sub.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/aggregation.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/control.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/default.yaml") }}
+  # {{ run("./reql_spec_generator", "spec/rql_test/src/range.yaml") }}
+end
+
+struct ReqlMatchers
+  def int_cmp(value)
+    IntCmpMatcher.new(value.to_i64)
+  end
+
+  def float_cmp(value)
+    FloatCmpMatcher.new(value.to_f64)
+  end
+
+  def uuid
+    UUIDMatcher.new
+  end
+
+  def arrlen(len, matcher)
+    ArrayMatcher.new(len, matcher)
+  end
+
+  def partial(matcher)
+    PartialMatcher.new(matcher)
+  end
+end
+
+abstract struct Matcher
+end
+
+struct IntCmpMatcher < Matcher
+  def initialize(@value : Int64)
+  end
+
+  def match(result)
+    result.should eq @value
+  end
+end
+
+struct FloatCmpMatcher < Matcher
+  def initialize(@value : Float64)
+  end
+
+  def match(result)
+    result.should eq @value
+  end
+end
+
+struct UUIDMatcher < Matcher
+  def match(result)
+    result.should be_a String
+    result.as_s.should Spec::MatchExpectation.new(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i)
+  end
+end
+
+struct ArrayMatcher(T) < Matcher
+  def initialize(@size : Int32, @matcher : T)
+  end
+
+  def match(result)
+    result.should be_a Array(ReQL::Datum::Type)
+    result.size.should eq @size
+    result.each do |value|
+      recursive_match value, @matcher
+    end
+  end
+end
+
+struct PartialMatcher(T) < Matcher
+  def initialize(@object : Hash(String, T))
+  end
+
+  def match(result)
+    result.should be_a Hash(String, ReQL::Datum::Type)
+    @object.keys.each do |key|
+      result.keys.includes?(key).should be_true
+      recursive_match result[key], @object[key]
+    end
+  end
+end
---
+++ spec/rql_test/reql_spec_generator.cr
@@ -0,0 +1,112 @@
+require "yaml"
+
+def yaml_fixes(str)
+  str = str.gsub("\\", "\\\\")
+  str = str.gsub(/(\w+): (.+)\n/) do
+    var = $1
+    value = $2
+    "#{var}: \"#{value.gsub("\"", "\\\"")}\"\n"
+  end
+  str
+end
+
+def quotes_fixes(str)
+  str = str.gsub(/'([^']*)'/) { "\"#{$1.gsub("\"", "\\\"")}\"" }
+  str
+end
+
+def language_fixes(str)
+  lang_replaces = {
+    "None"  => "nil",
+    "null"  => "nil",
+    "True"  => "true",
+    "False" => "false",
+  }
+  regex = /([^"'\w]|^)(#{lang_replaces.keys.join("|")})([^"'\w]|$)/
+  str = str.gsub(regex) do
+    "#{$1}#{lang_replaces[$2]}#{$3}"
+  end
+  str = quotes_fixes(str)
+  str = str.gsub("[]", "[] of Int32")
+  str = str.gsub(/([^\)\s]\s*){}([^"])/) { "#{$1}{} of String => Int32#{$2}" }
+  str = str.gsub(/^{}$/, "{} of String => Int32")
+  str = str.gsub(/([^\\\d])\":/) { "#{$1}\" => " }
+  str = str.gsub(/(\s|\{|,)(\d+):/) { "#{$1}#{$2} => " }
+  str = str.gsub(/(\}):/) { "#{$1} => " }
+  str = str.gsub(/(\W\s|\{|,|\()(\w+):/) { "#{$1}#{$2}: " }
+  str = str.gsub("nil:", "nil =>")
+  str = str.gsub("{{", "{ {")
+  str = str.gsub("orderby", "order_by")
+  str = str.gsub(/:(\w+) =>/) { "#{$1}:" }
+  str
+end
+
+data = YAML.parse(yaml_fixes File.read(ARGV[0]))
+
+puts "describe #{data["desc"].inspect} do"
+if tables = data["table_variable_name"]?
+  puts
+  tables.as_s.split(", ").map(&.split(" ")).flatten.each_with_index do |tablevar, i|
+    random_name = "test_#{Time.now.epoch}_#{rand(10000)}_#{i + 1}"
+    puts "  r.db(\"test\").table_create(#{random_name.inspect}).run"
+    puts "  #{tablevar} = r.db(\"test\").table(#{random_name.inspect})"
+  end
+end
+data["tests"].each_with_index do |test, i|
+  if d = test["def"]?
+    if d.raw.is_a? Hash
+      code = (d["rb"]? || d["cd"]).as_s
+      code = d["js"].as_s if d["js"]? && d["js"].as_s =~ /\* 1000/
+    else
+      code = d.as_s
+    end
+    puts "  #{language_fixes code}"
+  elsif test["ot"]? == nil && (test["rb"]? || test["cd"]?)
+    assign = (language_fixes (test["rb"]? || test["cd"]).as_s).split("=")
+    var = assign[0].strip
+    value = assign[1].strip
+    puts "  #{var} = #{value}.run.value.as(Int)"
+  else
+    test["ot"]?
+    subtests = test["rb"]? || test["cd"]?
+    next unless subtests
+    next if subtests == ""
+    subtests = [subtests] unless subtests.raw.is_a? Array
+    subtests = subtests.map &.as_s
+
+    output = test["ot"]
+    unless output.raw.is_a? String
+      if output["js"]? && output["js"].as_s =~ /reduction/
+        output = output["js"]
+      else
+        output = output["rb"]? || output["cd"]
+      end
+    end
+    output = quotes_fixes output.as_s
+
+    runopts = test["runopts"]? || "{} of String => String"
+
+    puts unless i == 0
+    subtests.each_with_index do |subtest, j|
+      next if output =~ /lambda/ || subtest =~ /lambda/
+      subtest = language_fixes subtest
+      puts unless j == 0
+      test_id = "##{i + 1}.#{j + 1}"
+      puts "  #{ARGV.includes?(test_id) ? "pending" : "it"} \"passes on test #{test_id}: #{subtest.gsub("\\", "\\\\").gsub("\"", "\\\"")}\" do"
+      if output =~ /err\("(\w+)",\s?"(.+?)"[,)]/
+        puts "    expect_raises(ReQL::#{$1}, \"#{$2.gsub("\\\\", "\\")}\") do"
+        puts "      (#{subtest}).run.value"
+        puts "    end"
+      elsif output =~ /err_regex\("(\w+)",\s?"(.+?)"[,)]/
+        puts "    expect_raises(ReQL::#{$1}, /#{$2.gsub("\\\\", "\\")}/) do"
+        puts "      (#{subtest}).run.value"
+        puts "    end"
+      else
+        puts "    result = (#{subtest}).run.value"
+        puts "    match_reql_output(result) { (#{language_fixes output}) }"
+      end
+      puts "  end"
+    end
+  end
+end
+puts "end"
