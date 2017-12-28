f6deda9474308f8e4f79cf7cb3b2ac412dd48166
Guilherme Bernal <dev@lbguilherme.com>
2017-12-27 22:53:38 -0300

    filter by object pattern

--- src/reql/terms/filter.cr
+++ src/reql/terms/filter.cr
@@ -8,20 +8,45 @@ module ReQL
     end
   end
 
+  def self.filter_pattern_match(obj, pattern)
+    return true if obj == pattern
+    return false unless obj.is_a? Hash
+    return false unless pattern.is_a? Hash
+
+    pattern.each do |(k, v)|
+      return false unless obj.has_key? k
+      return false unless filter_pattern_match(obj[k], v)
+    end
+
+    return true
+  end
+
   class Evaluator
     def eval(term : FilterTerm)
       target = eval term.args[0]
       func = eval term.args[1]
-      expect_type func, Func
+
+      block = case func
+      when Func
+        ->(val : Datum) {
+          func.as(Func).eval(self, val).datum
+        }
+      when Datum
+        ->(val : Datum) {
+          Datum.wrap(ReQL.filter_pattern_match(val.value, func.value))
+        }
+      else
+        raise QueryLogicError.new("Expected type FUNCTION but found #{func.class.reql_name}")
+      end
 
       case target
       when Stream
         FilterStream.new(target, ->(val : Datum) {
-          return func.as(Func).eval(self, val).datum
+          block.call(val)
         })
       when DatumArray
         DatumArray.new(target.value.select do |val|
-          func.as(Func).eval(self, Datum.wrap(val)).value.as(Datum::Type)
+          block.call(Datum.wrap(val)).value.as(Datum::Type)
         end)
       else
         raise QueryLogicError.new("Cannot convert #{target.class.reql_name} to SEQUENCE")
