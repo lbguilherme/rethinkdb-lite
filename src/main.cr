8f3e3f68f3db43554fd9021296fed4be8d5161ac
Guilherme Bernal <dev@lbguilherme.com>
2017-12-28 14:50:36 -0300

    Cleanup main.cr

--- src/main.cr
+++ src/main.cr
@@ -2,16 +2,7 @@ require "./storage/*"
 require "./server/*"
 require "./reql/*"
 
-require "file_utils"
-
-f = "/home/guilherme/test.db"
-
-if File.exists? f
-  FileUtils.rm f
-end
-if File.exists? "#{f}.wal"
-  FileUtils.rm "#{f}.wal"
-end
+include ReQL::DSL
 
 r.table_create("a").run
 r.table_create("b").run
@@ -24,22 +15,3 @@ Server::HttpServer.new(8080).start
 puts "Server #{Storage::Config.server_info.name.inspect} (#{Storage::Config.server_info.id}) ready."
 
 sleep
-
-# table = Table.create(f)
-
-# count = 0
-# s = Time.now
-# while true
-
-#   ss = Time.now
-#   1000.times do
-#     obj = {"id" => count += 1}
-#     table.insert(obj)
-#   end
-#   # p (Time.now-ss).to_f
-#   puts [count, Time.now-s, 1000.0/(Time.now-ss).to_f]
-# end
-
-include ReQL::DSL
-
-p r.table("weibc").get("odcbwo").run
