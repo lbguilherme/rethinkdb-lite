a100b677cb7d919a9552b395d382fc334010d239
Guilherme Bernal <dev@lbguilherme.com>
2017-12-29 13:52:40 -0300

    Implement inserting array of objects

--- src/reql/terms/insert.cr
+++ src/reql/terms/insert.cr
@@ -13,29 +13,54 @@ module ReQL
       table.check
 
       datum = eval term.args[1]
-      expect_type datum, Datum
+
+      docs = [] of Datum::Type
+      case datum
+      when DatumArray
+        docs = datum.value
+      when DatumObject
+        docs = [datum.value] of Datum::Type
+      else
+        raise QueryLogicError.new("Expected type OBJECT but found #{datum.class.reql_name}")
+      end
+
+      inserted = 0i64
+      errors = 0i64
+      generated_keys = nil
+      first_error = nil
+
+      docs.each do |obj|
+        begin
+          unless obj.is_a? Hash
+            raise QueryLogicError.new("Expected type OBJECT but found #{Datum.wrap(obj).class.reql_name}")
+          end
+          unless obj.has_key? "id"
+            id = UUID.random.to_s
+            obj["id"] = id
+            generated_keys = [] of Datum::Type unless generated_keys
+            generated_keys << id
+          end
+          table.insert(obj)
+        rescue err : RuntimeError
+          errors += 1
+          first_error ||= err.message
+        else
+          inserted += 1
+        end
+      end
 
       result = Hash(String, Datum::Type).new
       result["deleted"] = 0i64
       result["replaced"] = 0i64
       result["skipped"] = 0i64
       result["unchanged"] = 0i64
-
-      obj = datum.value.as(Hash)
-      unless obj.has_key? "id"
-        id = UUID.random.to_s
-        obj["id"] = id
-        result["generated_keys"] = Array(Datum::Type){id}
+      result["inserted"] = inserted
+      result["errors"] = errors
+      if first_error
+        result["first_error"] = first_error
       end
-      begin
-        table.insert(obj)
-      rescue err : RuntimeError
-        result["inserted"] = 0i64
-        result["errors"] = 1i64
-        result["first_error"] = err.message
-      else
-        result["inserted"] = 1i64
-        result["errors"] = 0i64
+      if generated_keys
+        result["generated_keys"] = generated_keys
       end
 
       Datum.wrap(result)
