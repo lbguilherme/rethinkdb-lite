278e0947276a6e554b25463b9adefa10fefd703a
Guilherme Bernal <dev@lbguilherme.com>
2018-01-02 17:03:41 -0300

    Fix travis script

--- travis_rethinkdb.sh
+++ travis_rethinkdb.sh
@@ -1,5 +1,5 @@
-source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
-wget -qO- http://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
-sudo apt-get update -qq
-sudo apt-get install rethinkdb -y --force-yes
+source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | tee /etc/apt/sources.list.d/rethinkdb.list
+curl http://download.rethinkdb.com/apt/pubkey.gpg | apt-key add -
+apt-get update -qq
+apt-get install rethinkdb -y --force-yes
 rethinkdb --daemon
