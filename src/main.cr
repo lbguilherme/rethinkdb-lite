7ca012e34731bee336d85bc6ad5f876cd7850911
Guilherme Bernal <dev@lbguilherme.com>
2018-01-14 15:11:38 -0300

    Undo changes to main

--- src/main.cr
+++ src/main.cr
@@ -6,27 +6,19 @@ include RethinkDB::DSL
 FileUtils.rm_rf "/tmp/rethinkdb-lite/data"
 conn = r.local_database("/tmp/rethinkdb-lite/data")
 
-# p r.js("1+2").run(conn)
+r.table_create("a").run(conn)
+r.table_create("b").run(conn)
+r.table_create("c").run(conn)
+r.db_create("foo").run(conn)
+r.db("foo").table_create("bar").run(conn)
+r.db("foo").table_create("baz").run(conn)
 
-p ReQL::Term.encode R.convert_type r.do(1, 2) { }, 10
-p ReQL::Term.encode R.convert_type r.do(1, 2, r.js("(function(a, b, c) { return c; })")), 10
-# p r.add.do{1}.run(conn)
-# p r(1).add(2).run(conn)
-# p r.add(1, 2).run(conn)
+RethinkDB::Server::HttpServer.new(8080, conn).start
+RethinkDB::Server::DriverServer.new(28015, conn).start
 
-# r.table_create("a").run(conn)
-# r.table_create("b").run(conn)
-# r.table_create("c").run(conn)
-# r.db_create("foo").run(conn)
-# r.db("foo").table_create("bar").run(conn)
-# r.db("foo").table_create("baz").run(conn)
+puts "Listening for administrative HTTP connections on http://localhost:8080/"
+puts "Listening for client driver connections on port 28015"
 
-# RethinkDB::Server::HttpServer.new(8080, conn).start
-# RethinkDB::Server::DriverServer.new(28015, conn).start
+# puts "Server #{Storage::Config.server_info.name.inspect} (#{Storage::Config.server_info.id}) ready."
 
-# puts "Listening for administrative HTTP connections on http://localhost:8080/"
-# puts "Listening for client driver connections on port 28015"
-
-# # puts "Server #{Storage::Config.server_info.name.inspect} (#{Storage::Config.server_info.id}) ready."
-
-# sleep
+sleep
