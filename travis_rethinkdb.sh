01b7d7b49532a47b9c8455d489020efa12ebd724
Guilherme Bernal <guilherme@cubos.io>
2019-12-22 21:48:26 -0300

    fix travis

--- travis_rethinkdb.sh
+++ travis_rethinkdb.sh
@@ -1,7 +1,7 @@
-source /etc/lsb-release && echo "deb https://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
+source /etc/lsb-release && echo "deb https://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | tee /etc/apt/sources.list.d/rethinkdb.list
 apt-get update -qq
 apt-get install wget -y --force-yes
-wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
+wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | apt-key add -
 apt-get update -qq
 apt-get install rethinkdb -y --force-yes
 rethinkdb --daemon
