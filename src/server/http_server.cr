428562446801e5ea200b9edf0f9d0435b63cf4ef
Guilherme Bernal <guilherme@cubos.io>
2019-12-25 16:54:50 -0300

    fix: http_server was missing exception backtrace

--- src/server/http_server.cr
+++ src/server/http_server.cr
@@ -45,6 +45,8 @@ module RethinkDB
                 "t" => answer["t"]?,
                 "r" => answer["r"]?,
                 "n" => answer["n"]?,
+                "e" => answer["e"]?,
+                "b" => answer["b"]?,
                 "p" => [{"duration(ms)" => (Time.utc - start).to_f * 1000}],
               }
             end
