a8a02ba6899ca6858a41ea927f19adf71f42959f
Guilherme Bernal <guilherme@cubos.io>
2019-12-29 11:36:18 -0300

    feat: graceful shutdown for storage engine

--- src/main.cr
+++ src/main.cr
@@ -12,4 +12,14 @@ puts "Listening for administrative HTTP connections on http://localhost:8080/"
 puts "Listening for client driver connections on port 28015"
 puts "Server #{conn.server["name"].inspect} (#{conn.server["id"]}) ready."
 
+Signal::INT.trap do
+  puts "Server got SIGINT"
+  exit
+end
+
+at_exit do
+  puts "Shutting down... (This may take a while if you had a lot of unflushed data in the writeback cache.)"
+  conn.close
+end
+
 sleep
