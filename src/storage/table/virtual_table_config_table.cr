2b81520195720a6a5742be3fa359991807fd3156
Guilherme Bernal <guilherme@cubos.io>
2019-12-30 00:45:14 -0300

    reimplement table_config as child of VirtualTable

--- src/storage/table/virtual_table_config_table.cr
+++ src/storage/table/virtual_table_config_table.cr
@@ -1,6 +1,7 @@
 module Storage
-  class VirtualTableConfigTable < AbstractTable
+  class VirtualTableConfigTable < VirtualTable
     def initialize(@manager : Manager)
+      super("table_config")
     end
 
     def replace(key)
@@ -8,27 +9,34 @@ module Storage
       raise "TODO"
     end
 
+    private def encode(info : KeyValueStore::TableInfo)
+      ReQL::Datum.new({
+        "db"          => @manager.kv.get_db(info.db).try &.name || info.db.to_s,
+        "durability"  => info.soft_durability ? "soft" : "hard",
+        "id"          => info.id.to_s,
+        "indexes"     => [] of String,
+        "name"        => info.name,
+        "primary_key" => info.primary_key,
+        "shards"      => [
+          {
+            "nonvoting_replicas" => [] of String,
+            "primary_replica"    => @manager.system_info.name,
+            "replicas"           => [@manager.system_info.name],
+          },
+        ],
+        "write_acks" => "single",
+        "write_hook" => nil,
+      }).hash_value
+    end
+
+    def get(key)
+      id = UUID.new(key.string_value) rescue return nil
+      encode(@manager.kv.get_table(id))
+    end
+
     def scan
-      @manager.databases.each_value do |db|
-        db.tables.each_value do |table|
-          yield ReQL::Datum.new({
-            "db"          => db.info.name,
-            "durability"  => table.soft_durability ? "soft" : "hard",
-            "id"          => table.id.to_s,
-            "indexes"     => [] of String,
-            "name"        => table.name,
-            "primary_key" => table.primary_key,
-            "shards"      => [
-              {
-                "nonvoting_replicas" => [] of String,
-                "primary_replica"    => @manager.system_info.name,
-                "replicas"           => [@manager.system_info.name],
-              },
-            ],
-            "write_acks" => "single",
-            "write_hook" => nil,
-          }).hash_value
-        end
+      @manager.kv.each_table do |info|
+        yield encode(info)
       end
     end
   end
