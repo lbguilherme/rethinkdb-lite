18452d866c9dc8bb8f8637e89b631181fafd9d25
Guilherme Bernal <guilherme@cubos.io>
2019-12-30 16:24:48 -0300

    refactor r.uuid with seed

--- src/reql/terms/uuid.cr
+++ src/reql/terms/uuid.cr
@@ -5,7 +5,7 @@ module ReQL
     register_type UUID
     prefix_inspect "uuid"
 
-    UUID_NAMESPACE = UUID.new "91461c99-f89d-49d2-af96-d8e2e14e9b58"
+    UUID_NAMESPACE = UUID.new("91461c99-f89d-49d2-af96-d8e2e14e9b58").bytes
 
     def compile
       expect_args(0, 1)
@@ -19,15 +19,15 @@ module ReQL
       else
         seed = eval(term.args[0]).string_value
 
-        bytes = Bytes.new(16 + seed.bytesize)
-        UuidTerm::UUID_NAMESPACE.bytes.to_slice.copy_to bytes.to_unsafe, 16
-        seed.to_unsafe.copy_to bytes.to_unsafe + 16, seed.bytesize
+        bytes = Digest::SHA1.digest do |ctx|
+          ctx.update UuidTerm::UUID_NAMESPACE
+          ctx.update seed
+        end
 
-        bytes = Digest::SHA1.digest(bytes).to_slice[0, 16]
         bytes[6] = ((bytes[6] & 0x0f) | 0x50)
         bytes[8] = ((bytes[8] & 0x3f) | 0x80)
 
-        Datum.new(UUID.new(bytes).to_s)
+        Datum.new(UUID.new(bytes.to_slice[0, 16]).to_s)
       end
     end
   end
