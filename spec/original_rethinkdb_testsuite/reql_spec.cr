abd81bc8f1eb1c7e09ee722cedb6c606b54973e8
Guilherme Bernal <guilherme@cubos.io>
2019-12-31 13:54:27 -0300

    fix spec after renaming dir

--- spec/original_rethinkdb_testsuite/reql_spec.cr
+++ spec/original_rethinkdb_testsuite/reql_spec.cr
@@ -7,31 +7,31 @@ include RethinkDB::DSL
 
 def run_reql_spec(conn)
   describe RethinkDB do
-    {{ run("./reql_spec_generator", "spec/rql_test/src/datum/array.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/datum/binary.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/datum/bool.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/datum/null.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/datum/number.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/datum/object.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/datum/string.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/datum/typeof.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/datum/uuid.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/add.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/aliases.yaml") }}
-    # {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/bit.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/comparison.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/div.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/floor_ceil_round.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/logic.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/math.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/mod.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/mul.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/math_logic/sub.yaml") }}
-    # {{ run("./reql_spec_generator", "spec/rql_test/src/aggregation.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/control.yaml") }}
-    # {{ run("./reql_spec_generator", "spec/rql_test/src/default.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/range.yaml") }}
-    {{ run("./reql_spec_generator", "spec/rql_test/src/transform/table.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/datum/array.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/datum/binary.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/datum/bool.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/datum/null.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/datum/number.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/datum/object.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/datum/string.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/datum/typeof.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/datum/uuid.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/math_logic/add.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/math_logic/aliases.yaml") }}
+    # {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/math_logic/bit.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/math_logic/comparison.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/math_logic/div.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/math_logic/floor_ceil_round.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/math_logic/logic.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/math_logic/math.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/math_logic/mod.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/math_logic/mul.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/math_logic/sub.yaml") }}
+    # {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/aggregation.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/control.yaml") }}
+    # {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/default.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/range.yaml") }}
+    {{ run("./reql_spec_generator", "spec/original_rethinkdb_testsuite/src/transform/table.yaml") }}
   end
 end
 
