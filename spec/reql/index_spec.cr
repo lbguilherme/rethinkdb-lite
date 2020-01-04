475f2dfd9b047c8c88dde442c12dd7f0f191c98b
Guilherme Bernal <guilherme@cubos.io>
2020-01-03 23:55:12 -0300

    format index_spec

--- spec/reql/index_spec.cr
+++ spec/reql/index_spec.cr
@@ -12,15 +12,14 @@ describe RethinkDB do
     r.table_create("test0").run(conn)
     r.table("test0").count.run(conn).should eq 0
 
-    r.table("test0").index_status().count.run(conn).should eq 0
+    r.table("test0").index_status.count.run(conn).should eq 0
     r.table("test0").index_create("field2").run(conn)
-    indexes = r.table("test0").index_status().run(conn).datum.array
+    indexes = r.table("test0").index_status.run(conn).datum.array
     indexes.size.should eq 1
     indexes[0].hash["index"].should eq "field2"
 
-    r.table("test0").index_list().run(conn).should eq ["field2"]
+    r.table("test0").index_list.run(conn).should eq ["field2"]
   end
-
 end
 
 Spec.after_suite { conn.close }
