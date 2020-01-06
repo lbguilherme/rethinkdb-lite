633ef173b6000519b96e71fdcc0dbb0540e9bfb1
Guilherme Bernal <guilherme@cubos.io>
2020-01-05 22:52:55 -0300

    implement sequence.concatMap

---
+++ src/reql/terms/concat_map.cr
@@ -0,0 +1,30 @@
+module ReQL
+  class ConcatMapTerm < Term
+    register_type CONCAT_MAP
+    infix_inspect "concat_map"
+
+    def compile
+      expect_args 2
+    end
+  end
+
+  class Evaluator
+    def eval(term : ConcatMapTerm)
+      target = eval(term.args[0])
+      func = eval(term.args[1]).as_function
+
+      case
+      when target.is_a? Stream
+        ConcatMapStream.new(target, ->(val : Datum) {
+          return func.eval(self, {val}).as_datum
+        })
+      when array = target.array_value?
+        Datum.new(array.flat_map do |val|
+          func.eval(self, {val}).array_value
+        end)
+      else
+        raise QueryLogicError.new("Cannot convert #{target.reql_type} to SEQUENCE")
+      end
+    end
+  end
+end
