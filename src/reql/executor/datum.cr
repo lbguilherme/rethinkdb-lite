992718440e112f5b2c19f19456a9eb09c7a4b4e6
Guilherme Bernal <guilherme@cubos.io>
2020-01-05 22:05:57 -0300

    optimize datum constructor

--- src/reql/executor/datum.cr
+++ src/reql/executor/datum.cr
@@ -10,6 +10,8 @@ module ReQL
       val = val.raw if val.is_a? JSON::Any
 
       @value = case val
+               when AbstractValue::Type
+                 val
                when AbstractValue
                  val.value
                when Array
