2f251085bee8120480aa89a2fbcb66beefc09070
Guilherme Bernal <guilherme@cubos.io>
2020-02-25 20:00:00 -0300

    waitgroup: fix race condition on multiple waiters

--- src/utils/waitgroup.cr
+++ src/utils/waitgroup.cr
@@ -20,7 +20,9 @@ class WaitGroup
 
   def wait : Nil
     return if @pending.get == 0
-    ch = @channel.set(Channel(Nil).new)
+    new_ch = Channel(Nil).new
+    old_ch = @channel.compare_and_set(nil, new_ch)[0]
+    ch = old_ch || new_ch
 
     # Note that another thread might zero @pending after the first check but
     # before the channel is set. Because of this we need to check again here.
