4921ab488274d0e0d2e043f6d715e431089a944c
Guilherme Bernal <guilherme@cubos.io>
2020-03-01 13:32:52 -0300

    Fix index_status with multiple indices

--- src/reql/terms/index_status.cr
+++ src/reql/terms/index_status.cr
@@ -15,29 +15,27 @@ module ReQL
       table = eval(term.args[0]).as_table
       storage = table.storage
 
-      case term.args.size
-      when 1
+      if term.args.size == 1
         if storage.is_a? Storage::PhysicalTable
           storage.get_all_indices_status
         else
           Datum.new([] of Datum)
         end
-      when 2
-        name = eval(term.args[1]).string_value
-
-        result = if storage.is_a? Storage::PhysicalTable
-                   storage.get_index_status(name)
-                 else
-                   nil
-                 end
-
-        if result.nil?
-          raise QueryLogicError.new("Index `#{name}` was not found on table `#{table.db.name}.#{table.name}`")
-        end
-
-        result
       else
-        raise "BUG: args should be 1..2"
+        Datum.new(eval(term.args[1]).array_or_set_value.map do |name|
+          name = name.string_value
+          result = if storage.is_a? Storage::PhysicalTable
+                     storage.get_index_status(name)
+                   else
+                     nil
+                   end
+
+          if result.nil?
+            raise QueryLogicError.new("Index `#{name}` was not found on table `#{table.db.name}.#{table.name}`")
+          end
+
+          result
+        end)
       end
     end
   end
