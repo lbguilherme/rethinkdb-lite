1b08eeed73af414bb1c33646b6878ffaf150ba78
Guilherme Bernal <guilherme@cubos.io>
2020-03-08 20:37:06 -0300

    optime r.group() when there is no aggregation

--- src/reql/terms/group.cr
+++ src/reql/terms/group.cr
@@ -13,60 +13,70 @@ module ReQL
     def eval_term(term : GroupTerm)
       target = eval(term.args[0])
       group_func = eval(term.args[1]).as_function
-      aggregation_func = term.args.size >= 3 ? eval(term.args[2]).as_function : nil
 
-      stream_table = {} of Datum => GroupStream
-      result_channel = Channel({Datum, Datum} | Exception).new
+      result = [] of Hash(String, Datum)
 
-      group_count = 0
+      if term.args.size == 3
+        aggregation_func = eval(term.args[2]).as_function
 
-      target.each do |value|
-        value = value.as_datum
-        group = group_func.eval(self, {value}).as_datum
-        stream = stream_table[group]?
-        unless stream
-          stream = stream_table[group] = GroupStream.new
-          group_count += 1
-          spawn do
-            begin
-              func = aggregation_func
-              if func
+        stream_table = {} of Datum => GroupStream
+        result_channel = Channel({Datum, Datum} | Exception).new
+
+        group_count = 0
+
+        target.each do |value|
+          value = value.as_datum
+          group = group_func.eval(self, {value}).as_datum
+          stream = stream_table[group]?
+          unless stream
+            stream = stream_table[group] = GroupStream.new
+            group_count += 1
+            spawn do
+              begin
                 evaluator = Evaluator.new(@manager, @worker)
                 evaluator.vars = @vars.dup
                 evaluator.now = @now
 
                 result_channel.send({
                   group,
-                  func.eval(evaluator, {stream.not_nil!}).as_datum,
+                  aggregation_func.eval(evaluator, {stream.not_nil!}).as_datum,
                 })
-              else
-                result_channel.send({
-                  group,
-                  stream.not_nil!.as_datum,
-                })
+              rescue exception
+                result_channel.send(exception)
               end
-            rescue exception
-              result_channel.send(exception)
             end
           end
-        end
-        stream.not_nil! << value
-      end
-
-      stream_table.each_value &.<<(nil)
-
-      result = [] of Hash(String, Datum)
-
-      group_count.times do
-        pair = result_channel.receive
-        if pair.is_a? Exception
-          raise pair
+          stream.not_nil! << value
         end
 
-        result << {
-          "group"     => pair[0],
-          "reduction" => pair[1],
-        }
+        stream_table.each_value &.<<(nil)
+
+        group_count.times do
+          pair = result_channel.receive
+          if pair.is_a? Exception
+            raise pair
+          end
+
+          result << {
+            "group"     => pair[0],
+            "reduction" => pair[1],
+          }
+        end
+      else
+        groups = Hash(Datum, Array(Datum)).new { |h, k| h[k] = [] of Datum }
+
+        target.each do |value|
+          value = value.as_datum
+          group = group_func.eval(self, {value}).as_datum
+          groups[group] << value
+        end
+
+        groups.each do |(group, array)|
+          result << {
+            "group"     => group,
+            "reduction" => Datum.new(array),
+          }
+        end
       end
 
       Datum.new(result)
