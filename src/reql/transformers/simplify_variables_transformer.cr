5ff5d3f5864486c378b98c760311dc8d7127b73d
Guilherme Bernal <guilherme@cubos.io>
2020-03-08 21:14:45 -0300

    Fix SimplifyVariablesTransformer

--- src/reql/transformers/simplify_variables_transformer.cr
+++ src/reql/transformers/simplify_variables_transformer.cr
@@ -1,34 +1,24 @@
 require "../term"
 require "./base"
 
-struct ReQL::SimplifyVariablesTransformer < ReQL::TransformerWithVisitor
-  @variables = Set(Int64).new
-  @mapping = {} of Int64 => Int64
+struct ReQL::SimplifyVariablesTransformer < ReQL::Transformer
+  @next_value = 0i64
+  @mapping : Hash(Int64, Int64)
+  @transformed_vars = Set(ReQL::VarTerm).new
+
+  def initialize(root_term : ReQL::Term::Type)
+    super(root_term)
+    @mapping = Hash(Int64, Int64).new { |h, k| h[k] = (@next_value += 1) }
+  end
 
   protected def should_transform(term : ReQL::FuncTerm)
     true
   end
 
-  protected def visit(term : ReQL::FuncTerm)
-    super(term)
-    term.args[0].as(ReQL::MakeArrayTerm).args.each do |arg|
-      @variables << arg.as(Int64)
-    end
-  end
-
-  protected def visit(term : ReQL::VarTerm)
-    super(term)
-    @variables << term.args[0].as(Int64)
-  end
-
-  protected def finish_visit
-    @variables.to_a.sort.each_with_index do |var, i|
-      @mapping[var] = (i + 1).to_i64
-    end
-  end
-
   protected def transform(term : ReQL::VarTerm) : ReQL::Term::Type
+    return term if @transformed_vars.includes?(term)
     term.args[0] = @mapping[term.args[0].as(Int64)]
+    @transformed_vars << term
     term
   end
 
