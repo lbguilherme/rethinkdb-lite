1fb3af1d5791ba4daea195f54af364ffbf95572d
Guilherme Bernal <guilherme@cubos.io>
2020-07-21 09:56:34 -0300

    fix: disable SimplifyVariablesTransformer

--- src/reql/transformers/transformer.cr
+++ src/reql/transformers/transformer.cr
@@ -3,6 +3,7 @@ require "./*"
 abstract struct ReQL::Transformer
   def self.transform(term : ReQL::Term::Type)
     term = ReQL::GroupTransformer.new(term).transform
-    term = ReQL::SimplifyVariablesTransformer.new(term).transform
+    # FIXME:
+    # term = ReQL::SimplifyVariablesTransformer.new(term).transform
   end
 end
